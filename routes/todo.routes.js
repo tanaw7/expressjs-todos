54af7ba7320f78eaf77072193cf00cfd42c16450
Tanapuch Wanwarang <tanapuch.wanwarang@acrolinx.com>
2024-08-22 17:49:46 +0700

    add route for get todo by id

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.post("/", todoController.createTodo);
 router.get("/", todoController.getTodos);
+router.get("/:todoId", todoController.getTodoById); // in controller it is req.params.todoId
 
 module.exports = router;
